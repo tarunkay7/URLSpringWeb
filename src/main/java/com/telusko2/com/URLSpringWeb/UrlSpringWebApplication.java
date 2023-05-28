5f87642a6cc636349258020b89d2ef937a85bc8d
= <=>
2023-05-28 16:35:29 +0530

    Initial commit

---
+++ src/main/java/com/telusko2/com/URLSpringWeb/UrlSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.telusko2.com.URLSpringWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlSpringWebApplication.class, args);
+	}
+
+}
